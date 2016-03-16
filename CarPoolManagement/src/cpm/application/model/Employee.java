ea91a184b64d896b891887fd276abb7805422a57
Isabel <Isabel@172.16.223.160>
2016-03-16 12:57:07 +0100

    hm

---
+++ CarPoolManagement/src/cpm/application/model/Employee.java
@@ -0,0 +1,42 @@
+package cpm.application.model;
+
+public class Employee extends app.MainApp {
+	
+	private String firstname;
+	private String lastname;
+	public  int personalnumber;	
+	private int licencenumber;
+
+	public Employee(String firstname, String lastname, int pn, int ln){ //Konstruktor
+		
+		this.firstname=firstname;
+		this.lastname = lastname;
+		this.personalnumber=pn;
+		this.licencenumber=ln;
+	}
+	
+	public void set_firstname(String firstname){
+		this.firstname=firstname;
+	}
+	public String get_firstname(){
+		return this.firstname;
+	}
+	public void set_lastname(String lastname){
+		this.lastname=lastname;
+	}
+	public String get_lastname(){
+		return this.lastname;
+	}		
+	public void set_personalnumber(int pn){
+		this.personalnumber= pn;
+	}
+	public int get_personalnumber(){
+		return this.personalnumber;	
+	}
+	public void set_licencenumber(int ln){
+		this.licencenumber= ln;
+	}
+	public int get_licencenumber(){
+		return this.licencenumber;
+	}
+}
